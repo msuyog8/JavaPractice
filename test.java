d330b9985fc1e0224c85268963aacd69c85e3933
Suyog Mayekar <114407875+msuyog8@users.noreply.github.com>
2022-10-30 12:38:35 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,20 @@
+class test{
+
+
+public static void main(String[] args)
+	string n1 = args[0];
+	int id= Integer.parseInt(args[1]);
+	double gr= Double.parseDouble(args[2]);
+	string ad= args[3];
+
+
+	var Undergrad = u1 Undergrad(n1,id,gr,ad);
+	
+	boolean	i= u1.isPassed();
+
+	if(i== true)
+		System.out.println("Undergrad student is Passed");
+	else
+
+		System.out.println("Undergrad student is Failed");
+}
